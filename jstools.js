4a04d00aa4d86a1e8ef132a4b64f35abae57610c
Yess <ahmedzezomossa@gmail.com>
2026-09-25 15:49:28 +0300

    Implement fastImportCSS for dynamic stylesheet loading
    Add fastImportCSS function for dynamic CSS imports.

---
+++ jstools.js
@@ -0,0 +1,29 @@
+async function fastImport(name, file) {
+  if (typeof file !== "string") throw new Error("Sorry, file needs to be a string!");
+  if (typeof name !== "string") throw new Error("Sorry, name needs to be a string!");
+  try {
+    const font = new FontFace(
+      name,
+      "url(https://ahmedac1.github.io/fontgo/v1/fonts/" + file + ".ttf)"
+    );
+
+    await font.load();
+    document.fonts.add(font);
+    return font;
+  } catch (err) {
+    throw new Error("Sorry, cannot fast import right now!");
+  }
+}
+
+async function fastImportCSS(name, file) {
+  if (typeof file !== "string") throw new Error("Sorry, file needs to be a string!");
+  if (typeof name !== "string") throw new Error("Sorry, name needs to be a string!");
+  try {
+    const link = document.createElement("link");
+
+    link.rel = "stylesheet";
+    link.href = im copy pasting the css code bro;
+  } catch (err) {
+    throw new Error("Sorry, cannot fast import right now!");
+  }
+}
